8c41ee17a9f0ae9410e6c00d881744b31ce39b52
MOHAMED ARRAFOUF <107930069+lfennn@users.noreply.github.com>
2023-06-22 00:37:10 +0100

    Done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints all alphabets in lowercase 10 time. * Return: On success 1.
+ * print_alphabet_x10 - prints all alphabets in lowercase 10 times.
+ *
+ * Return: On success 1.
  */
 void print_alphabet_x10(void)
 {
