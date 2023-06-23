9010a5d9dbabea012ee99ac5556b0cc325ecf4a4
MOHAMED ARRAFOUF <107930069+lfennn@users.noreply.github.com>
2023-06-23 22:50:19 +0100

    Done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,15 +5,6 @@
 * Return: 0
 */
 
-int positive_or_negative(int n){
-        if (n > 0)
-                printf("%i is positive\n", n);
-        else if (n == 0)
-                printf("%i is zero\n", n);
-        else
-                printf("%i is negative\n", n);
-}
-
 int main(void)
 {
         int i;
